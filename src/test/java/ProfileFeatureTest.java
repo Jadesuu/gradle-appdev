91e41c76339e9e13d0f6f2be532e054eb3840059
Otterly Cute <90382683+Jadesuu@users.noreply.github.com>
2022-12-13 02:51:50 +0800

    Init commit

---
+++ src/test/java/ProfileFeatureTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileFeatureTest {
+
+    @Test
+    void setStatus() {
+    }
+
+    @Test
+    void updateUsername() {
+    }
+}
